6dd0f2fd81bf4ab2819ee34c2a835d4fc687b6c1
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2023-06-27 04:02:32 +0200

    portal

--- 06-React/cinema/src/components/drop-down/drop-down.tsx
+++ 06-React/cinema/src/components/drop-down/drop-down.tsx
@@ -7,18 +7,25 @@ import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
 export const Dropdown = ({ placeholder, text, list, callback }: { placeholder: string, text: string, list: IFIlter[], callback: (value: string) => void }) => {
-  
+
   const ref = useRef<HTMLDivElement>(null);
   const [element, setElement] = useState<HTMLDivElement | null>(null);
   const [showDD, setShowDD] = useState(false);
+  const [coords, setCoords ] = useState({left: 0, width: 0, top: 0, height: 0})
 
   useEffect(() => {
     setElement(ref.current);
+    if (ref.current)
+    setCoords(ref.current.getBoundingClientRect())
   }, []);
 
   const handleClick = (value: string) => {
     callback(value);
   }
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=> setCoords(ref.current!.getBoundingClientRect()))
+  }, [])
 
   return <div>
     <p className={classNames(st.text_small)}>{text}</p>
@@ -26,16 +33,16 @@ export const Dropdown = ({ placeholder, text, list, callback }: { placeholder: s
       <div className={classNames(st.text_small, st.w_100, s.select)}>
         {placeholder}
       </div>
-      <span className={classNames(s.arrow_up, (showDD && s.arrow_down))}/>
+      <span className={classNames(s.arrow_up, (showDD && s.arrow_down))} />
       {element && showDD && (
-        <Options open={showDD} element={element} list={list} callback={(value) => handleClick(value)} close={() => setShowDD(false)} />
+        <Options open={showDD} coords={coords} list={list} callback={(value) => handleClick(value)} close={() => setShowDD(false)} />
       )}
     </div>
   </div>
 }
 
-const Options = (props: { open: boolean, list: IFIlter[], element: HTMLDivElement | null, callback: (value: string) => void, close: () => void }) => {
-  const { open, list, element, callback, close } = props;
+const Options = (props: { open: boolean, list: IFIlter[], coords: {left: number, width: number, top: number, height: number}, callback: (value: string) => void, close: () => void }) => {
+  const { open, list, coords, callback, close } = props;
   if (!open) {
     return null;
   }
@@ -48,8 +55,9 @@ const Options = (props: { open: boolean, list: IFIlter[], element: HTMLDivElemen
   ))
 
   return createPortal(
-    <div className={classNames(st.text_small, st.w_100, s.select, s.shadow)}>{options}
-    </div>,
-    element || document.body
+    <div className={classNames(st.text_small, st.w_100, s.select, s.shadow)} style={{ position: 'absolute', top: (coords.top || 0) + (coords.height || 0), width: coords.width, left: coords.left}}>{options}
+    </div>
+    ,
+    document.body
   )
 }
