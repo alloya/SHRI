b4a1639b0589fd9a8593f2d83353b2408e3e8a32
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2023-08-21 18:34:37 +0200

    select sort

--- algo/intense/js.js
+++ algo/intense/js.js
@@ -1,8 +1,42 @@
+const findMin = (arr, start) => {
+  let min = arr[start];
+  let ind = start;
+  for (let i = start; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+      ind = i;
+    }
+  }
+  return {min, ind}
+}
+
+const sortSelect = (arr) => {
+  let len = arr.length;
+  let iteration = 0;
+
+  while (iteration < len ) {
+    let min = findMin(arr, iteration)
+
+    let temp = arr[iteration];
+    arr[iteration] = min.min;
+    arr[min.ind] = temp
+
+    iteration++
+  }
+  
+
+  return arr
+}
+
+console.log(sortSelect([3, 4, 2, 5, 1, 7, 2]))
+
+
+
 const sort = (arr) => {
   let len = arr.length;
 
   while (len > 0) {
-    for (let i = 0; i < len; i++ ) {
+    for (let i = 0; i < len; i++) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i + 1];
         arr[i + 1] = arr[i];
@@ -13,5 +47,3 @@ const sort = (arr) => {
   }
   return arr
 }
-
-console.log(sort([1, 4, 2, 5, 3]))
